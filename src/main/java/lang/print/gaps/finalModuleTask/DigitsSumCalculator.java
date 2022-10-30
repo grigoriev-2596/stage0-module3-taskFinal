b848438f8954d6e8a0ee667e37c80371808a2842
grigoriev-2596 <grigoriev.egor2596@gmail.com>
2022-10-31 01:44:27 +0300

    all tasks are done

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int sum = 0;
+        do {
+            sum += number % 10;
+        } while((number = number / 10) > 0);
+        System.out.println(sum);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        StringBuilder reverseString = new StringBuilder();
+        do  {
+            reverseString.append(number % 10);
+        } while (((number = number / 10) > 0));
+        System.out.println(Integer.valueOf(reverseString.toString()));
     }
 }
